cab45a8dad0bbc40203922402e6f5b253501ef89
PeterCzingraber <cpeti73@gmail.com>
2025-02-26 17:43:03 +0100

    trial types

---
+++ mytrials.js
@@ -0,0 +1,58 @@
+var prime_probe_trials = [
+    {
+        prime: "le",
+        probe: "fel",
+        correct_response: "j",
+        congruency: "incongruent",
+        name: "vertical_incon_1"
+    },
+    {
+        prime: "fel",
+        probe: "le",
+        correct_response: "n",
+        congruency: "incongruent",
+        name: "vertical_incon_2"
+    },
+    {
+        prime: "le",
+        probe: "le",
+        correct_response: "n",
+        congruency: "congruent",
+        name: "vertical_con_1"
+    },
+    {
+        prime: "fel",
+        probe: "fel",
+        correct_response: "j",
+        congruency: "congruent",
+        name: "vertical_con_2"
+    },
+    {
+        prime: "bal",
+        probe: "jobb",
+        correct_response: "f",
+        congruency: "incongruent",
+        name: "horizontal_incon_1"
+    },
+    {
+        prime: "jobb",
+        probe: "bal",
+        correct_response: "g",
+        congruency: "incongruent",
+        name: "horizontal_incon_2"
+    },
+    {
+        prime: "bal",
+        probe: "bal",
+        correct_response: "f",
+        congruency: "congruent",
+        name: "horizontal_con_1"
+    },
+    {
+        prime: "jobb",
+        probe: "jobb",
+        correct_response: "g",
+        congruency: "congruent",
+        name: "horizontal_con_2"
+    }
+]
