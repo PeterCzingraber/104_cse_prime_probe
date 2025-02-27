966fbd3d624aec52ee469005a4c4523ea83fe2c7
PeterCzingraber <cpeti73@gmail.com>
2025-02-27 17:42:02 +0100

    Blocks are almost ready! trials are working! Our mood is constantly improving!

--- mytrials.js
+++ mytrials.js
@@ -1,56 +1,56 @@
 var prime_probe_trials = [
     {
-        prime: "le",
-        probe: "fel",
+        prime: "<span style='font-size: 35px;'>le<br>le<br>le</span>",
+        probe: "<span style='font-size: 35px;'>fel</span>",
         correct_response: "j",
         congruency: "incongruent",
         name: "vertical_incon_1"
     },
     {
-        prime: "fel",
-        probe: "le",
+        prime: "<span style='font-size: 35px;'>fel<br>fel<br>fel</span>",
+        probe: "<span style='font-size: 35px;'>le</span>",
         correct_response: "n",
         congruency: "incongruent",
         name: "vertical_incon_2"
     },
     {
-        prime: "le",
-        probe: "le",
+        prime: "<span style='font-size: 35px;'>le<br>le<br>le</span>",
+        probe: "<span style='font-size: 35px;'>le</span>",
         correct_response: "n",
         congruency: "congruent",
         name: "vertical_con_1"
     },
     {
-        prime: "fel",
-        probe: "fel",
+        prime: "<span style='font-size: 35px;'>fel<br>fel<br>fel</span>",
+        probe: "<span style='font-size: 35px;'>fel</span>",
         correct_response: "j",
         congruency: "congruent",
         name: "vertical_con_2"
     },
     {
-        prime: "bal",
-        probe: "jobb",
+        prime: "<span style='font-size: 35px;'>bal<br>bal<br>bal</span>",
+        probe: "<span style='font-size: 35px;'>jobb</span>",
         correct_response: "g",
         congruency: "incongruent",
         name: "horizontal_incon_1"
     },
     {
-        prime: "jobb",
-        probe: "bal",
+        prime: "<span style='font-size: 35px;'>jobb<br>jobb<br>jobb</span>",
+        probe: "<span style='font-size: 35px;'>bal</span>",
         correct_response: "f",
         congruency: "incongruent",
         name: "horizontal_incon_2"
     },
     {
-        prime: "bal",
-        probe: "bal",
+        prime: "<span style='font-size: 35px;'>bal<br>bal<br>bal</span>",
+        probe: "<span style='font-size: 35px;'>bal</span>",
         correct_response: "f",
         congruency: "congruent",
         name: "horizontal_con_1"
     },
     {
-        prime: "jobb",
-        probe: "jobb",
+        prime: "<span style='font-size: 35px;'>jobb<br>jobb<br>jobb</span>",
+        probe: "<span style='font-size: 35px;'>jobb</span>",
         correct_response: "g",
         congruency: "congruent",
         name: "horizontal_con_2"
