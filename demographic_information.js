19cdac48d1ad4f9ccd5fd95a81122a3246ffbd47
PeterCzingraber <cpeti73@gmail.com>
2025-03-24 14:20:42 +0100

    updated demographic questionnaire

--- demographic_information.js
+++ demographic_information.js
@@ -3,17 +3,28 @@ var education = {
     type: jsPsychSurveyMultiChoice,
     questions: [
       {
-        prompt: "Mi a legmagasabb iskolai végzettséged?",
+        prompt: "<strong>Mi a legmagasabb iskolai végzettséged?</strong>",
         name: "education",
         options: ["Általános iskola", "Szakközépiskola", "Szakgimnázium", "Gimnázium", "Főiskola", "Egyetem, alapképzés", "Egyetem, mesterképzés", "Doktori képzés"],
-        required: true
+        required: true,
         },
         {
-            prompt: "Melyik a nemmel azonosulsz?",
+            prompt: "<strong>Melyik a nemmel azonosulsz?</strong>",
             name: "gender",
             options: ["Férfi", "Nő", "Nem-bináris", "Gender-nonkomform", "Nem szeretném megosztani"],
             required: true
         }
     ],
+    button_label: "Tovább"
 };
-  var education_timeline = [education];
+
+var age_neptun = {
+  type: jsPsychSurveyText,
+  questions: [
+    {prompt: "Hány éves vagy?", name: "age", required: true},
+    {prompt: "Mi a NEPTUN kódod?", name: "neptun", required: true}
+  ],
+  button_label: "Tovább"
+};
+
+  var demographic_timeline = [age_neptun, education]
